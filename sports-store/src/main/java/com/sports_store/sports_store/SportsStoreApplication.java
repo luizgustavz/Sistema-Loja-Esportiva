3af4c474f340854aedf07a8785fc834c7ab79dd7
luizgustavz <luiz.gus.ms@gmail.com>
2025-09-10 19:06:01 -0300

    first commit

---
+++ sports-store/src/main/java/com/sports_store/sports_store/SportsStoreApplication.java
@@ -0,0 +1,13 @@
+package com.sports_store.sports_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsStoreApplication.class, args);
+	}
+
+}
---
+++ sports-store/src/test/java/com/sports_store/sports_store/SportsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sports_store.sports_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
