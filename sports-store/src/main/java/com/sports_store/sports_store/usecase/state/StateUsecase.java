c2a911bd3823ca67e2d5d9bb315ac6f6afb21c93
luizgustavz <luiz.gus.ms@gmail.com>
2025-09-11 18:55:53 -0300

    feat: cria os casos de uso, regras de negocios da entidade state com validacoes simples

---
+++ sports-store/src/main/java/com/sports_store/sports_store/usecase/state/StateUsecase.java
@@ -0,0 +1,51 @@
+package com.sports_store.sports_store.usecase.state;
+
+import com.sports_store.sports_store.domain.state.State;
+import com.sports_store.sports_store.repository.IStateRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StateUsecase {
+
+    private final IStateRepository repository;
+    public StateUsecase(IStateRepository IRepository){
+        this.repository = IRepository;
+    }
+
+    public State persist(State obj){
+        return repository.save(obj);
+    }
+
+    public State findById(Long id){
+        return repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException());
+    }
+
+    public State findByName(String name){
+        return repository.findByName(name)
+                .orElseThrow(() -> new IllegalArgumentException());
+    }
+
+    public List<State> findAll(){
+        return repository.findAll();
+    }
+    
+    public State patch(Long id, State obj){
+        if (!repository.existsById(id) || id == null){
+            throw new IllegalArgumentException();
+        }
+        obj.setId(id);
+        return repository.save(obj);
+    }
+
+    public void drop(Long id){
+        if (!repository.existsById(id)){
+            throw new IllegalArgumentException();
+        }
+        repository.deleteById(id);
+    }
+
+
+}
